d48459b733387920778df91d70ecee9cd1a0310b
Keyang Zhang <jxzhangkeyang@icloud.com>
2024-08-05 17:20:22 -0400

    Updated main

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import streamlit as st
 inventory_visualizer_page = st.Page("inventory_visualizer.py", title="Inventory Visualizer", icon=":material/inventory_2:")
 outbound_efficiency_page = st.Page("ob_efficiency.py", title="Single Day Outbound Efficiency", icon=":material/outbound:")
 multiple_outbound_efficiency_page = st.Page("multiple_ob_efficiency.py", title="Multi-Day Outbound Efficiency", icon=":material/outbound:")
+work_summary_page = st.Page("work_summary.py", title="Operator Work Summary", icon=":material/person_apron:")
 
-pg = st.navigation([inventory_visualizer_page, outbound_efficiency_page, multiple_outbound_efficiency_page])
+pg = st.navigation([inventory_visualizer_page, outbound_efficiency_page, multiple_outbound_efficiency_page, work_summary_page])
 st.set_page_config(page_title="WMS Utilities", page_icon=":material/monitoring:")
 pg.run()
