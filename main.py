6c15fbf1f1c2947bb951e5340a98c40b767aad5b
Keyang Zhang <jxzhangkeyang@icloud.com>
2024-08-08 12:10:08 -0400

    Added worker schedule entry point

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ outbound_efficiency_page = st.Page("ob_efficiency.py", title="Single Day Outboun
 multiple_outbound_efficiency_page = st.Page("multiple_ob_efficiency.py", title="Multi-Day Outbound Efficiency", icon=":material/outbound:")
 work_summary_page = st.Page("work_summary.py", title="Operator Work Summary", icon=":material/person_apron:")
 revenue_analysis_page = st.Page("revenue.py", title="Revenue Analysis", icon=":material/payments:")
+worker_schedule_page = st.Page("worker_schedule.py", title="Worker Schedule", icon=":material/edit_calendar:")
 
-pg = st.navigation([inventory_visualizer_page, outbound_efficiency_page, multiple_outbound_efficiency_page, work_summary_page, revenue_analysis_page])
+pg = st.navigation([inventory_visualizer_page,
+                    outbound_efficiency_page,
+                    multiple_outbound_efficiency_page,
+                    work_summary_page,
+                    revenue_analysis_page,
+                    worker_schedule_page])
 st.set_page_config(page_title="WMS Utilities", page_icon=":material/monitoring:")
 pg.run()
